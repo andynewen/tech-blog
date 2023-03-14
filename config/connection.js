2d735bbec345057672403067e66b62717d207426
andynewen <117054251+andynewen@users.noreply.github.com>
2023-03-14 19:39:52 +1100

    change code in connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,21 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+let sequelize;
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+// when deployed on Heroku
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    // localhost
+    sequelize = new Sequelize(process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
     });
+}
 
 module.exports = sequelize;
